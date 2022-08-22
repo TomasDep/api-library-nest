082509f47ccdda0416305c6b7e35810187437fb9
TomasDep <tomas.valenzuelacb@gmail.com>
2022-08-21 21:02:34 -0400

    [CHG] responses

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -14,32 +14,31 @@ import { PaginatioDto } from 'src/common/dtos/pagination.dto';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { Book } from './entities/book.entity';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
-  public create(@Body() createBookDto: CreateBookDto): Promise<Book> {
+  public create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
   @Get()
-  public findAll(@Query() paginationDto: PaginatioDto): Promise<Book[]> {
+  public findAll(@Query() paginationDto: PaginatioDto) {
     return this.booksService.findAll(paginationDto);
   }
 
   @Get(':term')
-  public findOne(@Param('term') term: string): Promise<Book> {
-    return this.booksService.findOne(term);
+  public findOne(@Param('term') term: string) {
+    return this.booksService.findOneBook(term);
   }
 
   @Patch(':id')
   public update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateBookDto: UpdateBookDto,
-  ): Promise<Book> {
+  ) {
     return this.booksService.update(id, updateBookDto);
   }
 
