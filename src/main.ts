ed7c255c70a407b905c2d7d956800515e3b85500
TomasDep <tomas.valenzuelacb@gmail.com>
2022-09-03 21:05:18 -0400

    [ADD] implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -12,6 +13,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Library RESTFul API')
+    .setDescription('This is an api to consult your favorite books')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(Number(process.env.PORT_API) || 3000);
 }
 bootstrap();
