5d573e1adb2812e16d86f82d45c889a4bce1cbcc
TomasDep <tomas.valenzuelacb@gmail.com>
2022-09-03 21:08:21 -0400

    [CHG] import auth module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -5,11 +5,12 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { Book } from './entities/book.entity';
 import { Genre } from './entities/genre.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
-  imports: [TypeOrmModule.forFeature([Book, Genre])],
+  imports: [TypeOrmModule.forFeature([Book, Genre]), AuthModule],
   exports: [BooksService],
 })
 export class BooksModule {}
