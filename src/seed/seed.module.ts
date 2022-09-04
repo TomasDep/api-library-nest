54b075a40ef1800804b29063269a2550a2582719
TomasDep <tomas.valenzuelacb@gmail.com>
2022-09-03 21:10:03 -0400

    [CHG] import user module

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { BooksModule } from 'src/books/books.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [BooksModule],
+  imports: [BooksModule, UsersModule],
 })
 export class SeedModule {}
