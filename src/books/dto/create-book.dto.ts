27357c18c8b1182bd6c36683b26c807c10437a9d
TomasDep <tomas.valenzuelacb@gmail.com>
2022-09-03 21:08:46 -0400

    [CHG] implements open api documentation

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import {
   IsString,
   MinLength,
@@ -10,29 +12,65 @@ import {
 } from 'class-validator';
 
 export class CreateBookDto {
+  @ApiProperty({
+    description: 'Product title',
+    uniqueItems: true,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   name: string;
+  @ApiProperty({
+    description: 'Product description',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description?: string;
+  @ApiProperty({
+    description: 'Product observations',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   observations?: string;
+  @ApiProperty({
+    description: 'Product stock',
+    nullable: true,
+    default: 0,
+  })
   @IsInt()
   @IsOptional()
   @IsPositive()
   stock?: number;
+  @ApiProperty({
+    description: 'Product status',
+    nullable: true,
+    default: false,
+  })
   @IsBoolean()
   @IsOptional()
   status?: boolean;
+  @ApiProperty({
+    description: 'Product price',
+    nullable: true,
+    default: 0,
+  })
   @IsNumber()
   @IsOptional()
   @IsPositive()
   price?: number;
+  @ApiProperty({
+    description: 'Product slug',
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
+  @ApiProperty({
+    description: 'Product genres',
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   genres: string[];
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,36 +1,90 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import {
   BeforeInsert,
   BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
 import { Genre } from './genre.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity('books')
 export class Book {
+  @ApiProperty({
+    example: 'cd533345-f1f3-48c9-a62e-7dc2da50c8f8',
+    description: 'Book id',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @ApiProperty({
+    example: 'Fire and Blood',
+    description: 'Book title',
+  })
   @Column('text')
   name: string;
+  @ApiProperty({
+    example:
+      'Centuries before A Game of Thrones, an even greater game began, one that set the skies' +
+      'alight with dragon flame and saw the Seven Kingdoms turned to ash.',
+    description: 'Book description',
+    nullable: true,
+  })
   @Column('text', { nullable: true })
   description: string;
+  @ApiProperty({
+    example: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Book observations',
+    nullable: true,
+  })
   @Column('text', { nullable: true })
   observations: string;
+  @ApiProperty({
+    example: 10,
+    description: 'Book stock',
+    nullable: true,
+  })
   @Column('int', { default: 0 })
   stock: number;
+  @ApiProperty({
+    example: true,
+    description: 'Book status',
+    nullable: true,
+    default: 0,
+  })
   @Column('boolean', { default: false })
   status: boolean;
+  @ApiProperty({
+    example: 26.9,
+    description: 'Book price',
+    nullable: true,
+    default: false,
+  })
   @Column('double precision', { default: 0 })
   price: number;
+  @ApiProperty({
+    example: 'fire-and-blood',
+    description: 'Book slug',
+    nullable: true,
+    default: false,
+  })
   @Column('text', { unique: true })
   slug: string;
+  @ApiProperty({
+    example: ['fantasy'],
+    description: 'Book genres',
+  })
   @OneToMany(() => Genre, (genre) => genre.book, { cascade: true, eager: true })
   genres: Genre[];
+  @ManyToOne(() => User, (user) => user.books, { eager: true })
+  user: User;
   @CreateDateColumn({
     type: 'timestamp',
     name: 'created_at',
